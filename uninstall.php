812108482a718eb5410a2c629b4f4202f8d6bec6
Prabal Mallick <prabalsslw@gmail.com>
2020-05-23 16:33:25 +0600

    Added Woocommerce Order Alert

---
+++ uninstall.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+ * Removes options from database when plugin is deleted.
+ *  
+ *
+ */
+
+# if uninstall not called from WordPress exit
+
+	if (!defined('WP_UNINSTALL_PLUGIN' ))
+	    exit();
+
+	global $wpdb, $wp_version;
+
+	// $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}safeguard_otp" );
+	// $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}safeguard_wooalert" );
+
+	// delete_option("safeg_db_version");
+	// delete_option('safeg_setting');
+
+	wp_cache_flush();
+
+?>
